57f7ead54397ee816c7fa045ceb3fd7f193e317a
Sandaru Divyantha <79299187+sandarudivyantha@users.noreply.github.com>
2025-02-02 16:40:41 +0530

    for data feching server API created

--- server/server.js
+++ server/server.js
@@ -14,6 +14,14 @@ app.get("/", (req, res) => {
 
 // API
 app.get("/api", (req, res) => res.status(200).json(data));
+app.get("/api/:id", (req, res) => {
+  const filteredData = data.find((ele) => ele.id === Number(req.params.id));
+  if (filteredData !== undefined) {
+    res.status(200).json(filteredData);
+  } else {
+    res.status(404).json({ message: `No data found for id: ${req.params.id}` });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);

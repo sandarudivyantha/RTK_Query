0f539abdc4b84a9018808186a1ed8422d7f54932
Sandaru Divyantha <79299187+sandarudivyantha@users.noreply.github.com>
2025-02-02 16:29:43 +0530

    server file created. db json connected to the server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-const data = require('./db/db.json');
+const express = require("express");
+const cors = require("cors");
+const data = require("./db/db.json");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -8,13 +8,13 @@ const PORT = process.env.PORT || 5000;
 app.use(express());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello, this is the RTK Query server!');
+app.get("/", (req, res) => {
+  res.send("Hello, this is the RTK Query server!");
 });
 
 // API
-
+app.get("/api", (req, res) => res.status(200).json(data));
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
